7f9c4709cd6b3bbd79c308e230cdbdf286edb069
romaing <rocambille@gmail.com>
2024-12-25 12:53:14 +0100

    tweaked server code

--- server.ts
+++ server.ts
@@ -1,57 +1,26 @@
 import express from "express";
+import type { Express } from "express";
 import fs from "node:fs";
 import path from "node:path";
-import { Transform } from "node:stream";
 import { fileURLToPath } from "node:url";
 import { createServer as createViteServer } from "vite";
-import type { ViteDevServer } from "vite";
 
 import "./src/database/checkConnection";
 
-import expressRouter from "./src/express/router";
+createServer().then((server) => {
+  const port = 5173;
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const isProduction = process.env.NODE_ENV === "production";
-
-// 1. Read index.html
-const indexHtml = fs.readFileSync(
-  path.resolve(
-    __dirname,
-    isProduction ? "dist/client/index.html" : "index.html",
-  ),
-  "utf-8",
-);
+  server.listen(port, () => {
+    console.info(`Listening on port ${port}`);
+  });
+});
 
 async function createServer() {
   const app = express();
 
-  app.use(expressRouter);
+  app.use((await import("./src/express/router")).default);
 
-  let vite = null as ViteDevServer | null;
-
-  if (isProduction) {
-    const compression = (await import("compression")).default;
-    const sirv = (await import("sirv")).default;
-    app.use(compression());
-    app.use(sirv("./dist/client", { extensions: [] }));
-  } else {
-    // Create Vite server in middleware mode and configure the app type as
-    // 'custom', disabling Vite's own HTML serving logic so parent server
-    // can take control
-    vite = await createViteServer({
-      server: { middlewareMode: true },
-      appType: "custom",
-    });
-
-    // Use vite's connect instance as middleware. If you use your own
-    // express router (express.Router()), you should use router.use
-    // When the server restarts (for example after the user modifies
-    // vite.config.js), `vite.middlewares` is still going to be the same
-    // reference (with a new internal stack of Vite and plugin-injected
-    // middlewares). The following is valid even after restarts.
-    app.use(vite.middlewares);
-  }
+  const maybeVite = await configure(app);
 
   app.use("*", async (req, res, next) => {
     const url = req.originalUrl;
@@ -63,87 +32,104 @@ async function createServer() {
     // * on the server (and build phase), it will refer to the fetch API from node
     //   => fetch("/api") is not working
 
-    // We're using node API here and need to fix fetch for non-absolute urls :
+    // SSR is using node API here, so we need to fix fetch for non-absolute urls :
     {
       const nodeFetch = globalThis.fetch;
 
-      globalThis.fetch = (url) => {
-        if (url.includes("://")) {
-          return nodeFetch(url);
+      globalThis.fetch = (resource) => {
+        if (resource.includes("://")) {
+          return nodeFetch(resource);
         }
 
-        return nodeFetch(`${req.protocol}://${req.get("host")}${url}`);
+        if (resource.startsWith("/")) {
+          return nodeFetch(`${req.protocol}://${req.get("host")}${resource}`);
+        }
+
+        return nodeFetch(
+          `${req.protocol}://${req.get("host")}${url}${resource}`,
+        );
       };
     }
 
     try {
+      const indexHtml = readIndexHtml();
+
       const getTemplateAndRender = async () => {
-        if (isProduction) {
+        if (maybeVite == null) {
           const render = (await import("./dist/server/entry-server")).render;
           return { template: indexHtml, render };
         }
 
-        // 2. Apply Vite HTML transforms. This injects the Vite HMR client,
+        const vite = maybeVite;
+
+        // 1. Apply Vite HTML transforms. This injects the Vite HMR client,
         //    and also applies HTML transforms from Vite plugins, e.g. global
         //    preambles from @vitejs/plugin-react
-        const template = await (vite as ViteDevServer).transformIndexHtml(
-          url,
-          indexHtml,
-        );
+        const template = await vite.transformIndexHtml(url, indexHtml);
 
-        // 3. Load the server entry. ssrLoadModule automatically transforms
+        // 2. Load the server entry. ssrLoadModule automatically transforms
         //    ESM source code to be usable in Node.js! There is no bundling
         //    required, and provides efficient invalidation similar to HMR.
-        const render = (
-          await (vite as ViteDevServer).ssrLoadModule("/src/entry-server")
-        ).render;
+        const render = (await vite.ssrLoadModule("/src/entry-server")).render;
 
         return { template, render };
       };
 
       const { template, render } = await getTemplateAndRender();
 
-      // 4. render the app HTML. This assumes entry-server.js's exported
+      // 3. render the app HTML. This assumes entry-server.js's exported
       //     `render` function calls appropriate framework SSR APIs,
       //    e.g. ReactDOMServer.renderToString()
-      const { pipe } = await render(
-        `${req.protocol}://${req.get("host")}${req.originalUrl}`,
-      );
-
-      res.status(200);
-
-      res.set({ "Content-Type": "text/html" });
-
-      const transformStream = new Transform({
-        transform(chunk, encoding, callback) {
-          res.write(chunk, encoding);
-          callback();
-        },
-      });
-
-      // 5. Inject the app-rendered HTML into the template.
-      const [htmlStart, htmlEnd] = template.split("<!--ssr-outlet-->");
-
-      res.write(htmlStart);
-
-      transformStream.on("finish", () => {
-        res.end(htmlEnd);
-      });
-
-      pipe(transformStream);
+      await render(template, req, res);
     } catch (error) {
       // If an error is caught, let Vite fix the stack trace so it maps back
       // to your actual source code.
-      vite?.ssrFixStacktrace(error as Error);
+      maybeVite?.ssrFixStacktrace(error as Error);
       next(error);
     }
   });
 
-  const port = 5173;
-
-  app.listen(port, () => {
-    console.info(`Listening on port ${port}`);
-  });
+  return app;
 }
 
-createServer();
+const isProduction = process.env.NODE_ENV === "production";
+
+function readIndexHtml() {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  return fs.readFileSync(
+    path.resolve(
+      __dirname,
+      isProduction ? "dist/client/index.html" : "index.html",
+    ),
+    "utf-8",
+  );
+}
+
+async function configure(app: Express) {
+  if (isProduction) {
+    const compression = (await import("compression")).default;
+    const sirv = (await import("sirv")).default;
+
+    app.use(compression());
+    app.use(sirv("./dist/client", { extensions: [] }));
+  } else {
+    // Create Vite server in middleware mode and configure the app type as
+    // 'custom', disabling Vite's own HTML serving logic so parent server
+    // can take control
+    const vite = await createViteServer({
+      server: { middlewareMode: true },
+      appType: "custom",
+    });
+
+    // Use vite's connect instance as middleware. If you use your own
+    // express router (express.Router()), you should use router.use
+    // When the server restarts (for example after the user modifies
+    // vite.config.js), `vite.middlewares` is still going to be the same
+    // reference (with a new internal stack of Vite and plugin-injected
+    // middlewares). The following is valid even after restarts.
+    app.use(vite.middlewares);
+
+    return vite;
+  }
+}
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,6 +1,7 @@
+import type { Request, Response } from "express";
+import { Transform } from "node:stream";
 import { StrictMode } from "react";
 import { renderToPipeableStream } from "react-dom/server";
-import type { RenderToPipeableStreamOptions } from "react-dom/server";
 import {
   createStaticHandler,
   createStaticRouter,
@@ -9,26 +10,44 @@ import {
 
 import routes from "./react/routes";
 
-export const render = async (
-  fullUrl: string,
-  options?: RenderToPipeableStreamOptions,
-) => {
+export const render = async (template: string, req: Request, res: Response) => {
   const { query, dataRoutes } = createStaticHandler(routes);
 
-  const context = await query(new Request(fullUrl));
+  const context = await query(
+    new Request(`${req.protocol}://${req.get("host")}${req.originalUrl}`),
+  );
 
   if (context instanceof Response) {
     throw new Error("React Router stuff to handle");
-
-    // return context;
   }
 
   const router = createStaticRouter(dataRoutes, context);
 
-  return renderToPipeableStream(
+  const { pipe } = renderToPipeableStream(
     <StrictMode>
       <StaticRouterProvider router={router} context={context} />
     </StrictMode>,
-    options,
   );
+
+  res.status(200);
+
+  res.set({ "Content-Type": "text/html" });
+
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      res.write(chunk, encoding);
+      callback();
+    },
+  });
+
+  // 5. Inject the app-rendered HTML into the template.
+  const [htmlStart, htmlEnd] = template.split("<!--ssr-outlet-->");
+
+  res.write(htmlStart);
+
+  transformStream.on("finish", () => {
+    res.end(htmlEnd);
+  });
+
+  pipe(transformStream);
 };
