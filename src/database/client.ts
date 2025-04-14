9ec6c51e6aa271bf0271c15799f5e419ef53ca78
romaing <rocambille@gmail.com>
2025-04-14 16:12:31 +0200

    minor cosmetic changes after writing the wiki

--- src/database/client.ts
+++ src/database/client.ts
@@ -4,9 +4,13 @@ const { MYSQL_ROOT_PASSWORD, MYSQL_DATABASE } = process.env;
 // Create a connection pool to the database
 import mysql from "mysql2/promise";
 
+/* ************************************************************************ */
+
 const client = mysql.createPool(
   `mysql://root:${MYSQL_ROOT_PASSWORD}@database:3306/${MYSQL_DATABASE}`,
 );
+
+/* ************************************************************************ */
 
 // Ready to export
 export default client;
